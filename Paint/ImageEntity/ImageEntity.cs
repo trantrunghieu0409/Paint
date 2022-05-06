89343cbbbc91b19d1272272ed769da970540825b
Trần Quốc Tuấn <56530679+TranQuocTuan1711@users.noreply.github.com>
2022-05-06 22:24:30 +0700

    ImageEntity

---
+++ Paint/ImageEntity/ImageEntity.cs
@@ -0,0 +1,77 @@
+﻿using IContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageEntity
+{
+    public class ImageEntity : IShapeEntity, ICloneable
+    {
+        public Point TopLeft { get; set; }
+        public float Width;
+        public float Height;
+
+        public string Name => "Image";
+
+        public BitmapImage Icon => new BitmapImage(new Uri("Images/insert.png", UriKind.Relative));
+
+        public BitmapImage Image;
+
+        public SolidColorBrush Brush { get; set; }
+        public SolidColorBrush Background { get; set; }
+        public int Thickness { get; set; }
+        public DoubleCollection StrokeDash { get; set; }
+
+        
+        public void HandleStart(Point point)
+        {
+            TopLeft = point;
+        }
+        public void HandleEnd(Point point)
+        {
+            
+        }
+
+        public void HandleSolidColorBrush(SolidColorBrush brush)
+        {
+            Brush = brush;
+        }
+        public void HandleThickness(int thickness)
+        {
+            Thickness = thickness;
+        }
+        public void HandleDoubleCollection(DoubleCollection dash)
+        {
+            StrokeDash = dash;
+        }
+
+        public void HandleBackground(SolidColorBrush background)
+        {
+            Background = background;
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+        public bool isHovering(double x, double y)
+        {
+            return false;
+        }
+
+        public void pasteShape(Point startPoint, IShapeEntity shape)
+        {
+            var element = shape as ImageEntity;
+
+            TopLeft = startPoint;
+            
+        }
+
+
+    }
+}
